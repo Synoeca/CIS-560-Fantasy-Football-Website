5c43f0a3caacdf54f7bf0fa5a7f22e5d4b8243e9
Synoeca <synoeca523@ksu.edu>
2024-11-14 16:18:29 -0600

    Update `_Layout.cshtml` and `Player.cshtml`.

--- Web App/Pages/Player.cshtml.cs
+++ Web App/Pages/Player.cshtml.cs
@@ -9,18 +9,26 @@ namespace Web_App.Pages
     {
         private readonly ILogger<PlayerModel> _logger;
         private readonly PlayerRepository _playerRepository;
+        public TeamRepository TeamRepository { get; set; }
         public List<Player> Players { get; set; } = [];
+        public List<Team> Teams { get; set; } = [];
 
         public PlayerModel(ILogger<PlayerModel> logger, IConfiguration configuration)
         {
             _logger = logger;
             string? connectionString = configuration.GetConnectionString("DefaultConnection");
-            if (connectionString != null) _playerRepository = new PlayerRepository(connectionString);
+            if (connectionString != null)
+            {
+                _playerRepository = new PlayerRepository(connectionString);
+                TeamRepository = new TeamRepository(connectionString);
+            }
+               
         }
 
         public void OnGet()
         {
             Players = _playerRepository.GetAllPlayers().ToList();
+            Teams = TeamRepository.GetAllTeams().ToList();
         }
     }
 }
